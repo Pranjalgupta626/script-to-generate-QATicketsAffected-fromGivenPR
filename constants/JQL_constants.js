c5132eed1da59769f142db42db5d94e17afecff3
Pranjal Gupta <71770902+Pranjalgupta626@users.noreply.github.com>
2021-03-26 10:17:10 +0530

    Create JQL_constants.js

---
+++ constants/JQL_constants.js
@@ -0,0 +1,7 @@
+const JQL_CONSTANTS = {
+    FarmCareReadyForQAS19: "project='FarmCare' AND sprint='RT Sprint 19' AND status='Ready for QA'",
+    FarmCareS19: "sprint='RT Sprint 19' AND status='Ready for QA'"
+}
+
+// export { JQL_CONSTANTS }
+module.exports = JQL_CONSTANTS;
